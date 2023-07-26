e5d5efd0e5b0e210ba25e0c9ecdfb24495af8101
JunWedi <junwedisinghe@gmail.com>
2023-07-27 00:26:26 +0900

    材料と作り方の登録実装

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -16,6 +16,28 @@
                 <h2>Body</h2>
                 <textarea name="recipe[body]" placeholder="紹介文"></textarea>
             </div>
+            <div id="steps">
+                <h2>Step</h2>
+                <div class="step">    
+                 <input type="number" name="step_numbers[]" placeholder="ステップ番号">
+                 <textarea name="step_descriptions[]" placeholder="ステップの説明"></textarea>
+                 <button type="button" onclick="removeIngredient(this)">削除</button>
+                </div>
+            </div>
+
+            <button type="button" onclick="addStep()">ステップを追加</button>
+
+            <div id="ingredients">
+                <h2>Ingredients</h2>
+                <div class="ingredient">
+                    <input type="text" name="ingredient_names[]" placeholder="材料名">
+                    <input type="text" name="ingredient_quantities[]" placeholder="量">
+                    <input type="text" name="ingredient_units[]" placeholder="単位">
+                    <button type="button" onclick="removeIngredient(this)">削除</button>
+                </div>
+            </div>
+
+            <button type="button" onclick="addIngredient()">材料を追加</button>
             <div class="category">
               <h2>Category</h2>
                 <select name="category_id">
@@ -26,8 +48,9 @@
                  @endforeach
                 </select>
             </div>
+            <h2>Tag</h2>
             <div class="tag">
-                <textarea name="tag_id" placeholder="辛い、冷たい、etc"></textarea>
+                <textarea name="tag_id" placeholder="#ちょい辛い#一度は食べたいetc"></textarea>
             </div>
             <input type="submit" value="store"/>
         </form>
@@ -36,3 +59,30 @@
         </div>
     </body>
 </html>
+
+<script>
+function addIngredient() {
+    var div = document.createElement('div');
+    div.className = 'ingredient';
+    div.innerHTML = '<input type="text" name="ingredient_names[]" placeholder="材料名"><input type="text" name="ingredient_quantities[]" placeholder="量"><input type="text" name="ingredient_units[]" placeholder="単位"><button type="button" onclick="removeIngredient(this)">削除</button>';
+    document.getElementById('ingredients').appendChild(div);
+}
+
+function removeIngredient(button) {
+    var ingredient = button.parentNode;
+    ingredient.parentNode.removeChild(ingredient);
+}
+
+function addStep() {
+    var div = document.createElement('div');
+    div.className = 'step';
+    div.innerHTML = '<input type="number" name="step_numbers[]" placeholder="ステップ番号"><textarea name="step_descriptions[]" placeholder="ステップの説明"></textarea><button type="button" onclick="removeStep(this)">削除</button>';
+    document.getElementById('steps').appendChild(div);
+}
+
+function removeStep(button) {
+    var step = button.parentNode;
+    step.parentNode.removeChild(step);
+}
+
+</script>
